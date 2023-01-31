843b7a0b1b32ab998300c33d57cf1e64f6325cf7
alexbalak21 <alex.balak@outlook.com>
2023-01-31 20:53:45 +0100

    Structured Project

--- src/main/java/com/example/alex/controller/TodoController.java
+++ src/main/java/com/example/alex/controller/TodoController.java
@@ -1,2 +1,18 @@
-package com.example.alex.controller;public class TodoController {
+package com.example.alex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Home";
+    }
+    @GetMapping("/{id}")
+    public String getOne(){
+        return "Return Todo";
+    }
 }
