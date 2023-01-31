f588e6f178f2aa2030687727b9de25a2350c1fad
alexbalak21 <alex.balak@outlook.com>
2023-01-31 22:39:03 +0100

    Initial 2

--- src/main/java/com/example/alex/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.alex.repository;
-
-import com.example.alex.model.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository <Task, Integer> {
-}
